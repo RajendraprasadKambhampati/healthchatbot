a6f3f771f8e23179d5a61768a205e4feac07de0f
Mangireddygari-Reshma <reetumangireddy@gmail.com>
2025-02-28 07:54:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app  # ✅ Import from the app package
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
